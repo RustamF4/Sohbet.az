2288dd91a6254a51925f585dbf47a4a524db32dd
RustamF4 <RustamEf@outlook.com>
2024-04-24 08:44:04 +0400

    create profile model

--- social/models.py
+++ social/models.py
@@ -15,3 +15,10 @@ class Comments(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, verbose_name='user', related_name='profile', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, blank=True, null=True)
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='media/uploads/profile_pictures', default='media/uploads/profile_pictures/default.png', blank=True)
